66ad8bf51a103e37e169d113d61716e098b2bfe2
Pranav Sindura <pranav.sindura@gmail.com>
2019-06-12 13:27:21 +0530

    data

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+export default class About extends Component {
+    render() {
+        return (
+            <div>
+                <Header selected={"About"}/>
+                    <h1>About Page Not Done Yet</h1>
+                <Footer />
+            </div>
+        )
+    }
+}
